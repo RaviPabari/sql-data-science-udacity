52c50c3230202f865478b9cb6f4b94890741e3f8
RaviPabari <ravipabari@outlook.com>
2020-07-08 12:36:26 +0530

    WITH + subquery

--- 4) SQL Subquerries & Temporary Tables/2)More on subquerries.sql
+++ 4) SQL Subquerries & Temporary Tables/2)More on subquerries.sql
@@ -1,2 +1,8 @@
 --If you want to pull out all those orders which were placced in the same month
 --when the first ever parch and posey order was placced
+SELECT *
+FROM orders
+WHERE DATE_TRUNC('month',occurred_at) =
+(SELECT DATE_TRUNC('month',MIN(occurred_at)) AS min_month
+FROM orders)
+ORDER BY occurred_at;
